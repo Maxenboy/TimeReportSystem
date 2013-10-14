0ba0cde74bd1327891ee09ba9530dda5b1f5dd5c
Andreas Olsson <olsson.andreas@gmail.com>
2013-10-14 12:52:38 +0200

    Implemented ProjectGroup.java

--- src/database/ProjectGroup.java
+++ src/database/ProjectGroup.java
@@ -2,4 +2,70 @@ package database;
 
 public class ProjectGroup {
 
+	public int id;
+	public String projectName;
+	public boolean active;
+	public int startWeek;
+	public int endWeek;
+	public int estimatedTime;
+
+	public ProjectGroup(int id, String projectName, boolean active,
+			int startWeek, int endWeek, int estimatedTime) {
+		super();
+		this.id = id;
+		this.projectName = projectName;
+		this.active = active;
+		this.startWeek = startWeek;
+		this.endWeek = endWeek;
+		this.estimatedTime = estimatedTime;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public int getStartWeek() {
+		return startWeek;
+	}
+
+	public void setStartWeek(int startWeek) {
+		this.startWeek = startWeek;
+	}
+
+	public int getEndWeek() {
+		return endWeek;
+	}
+
+	public void setEndWeek(int endWeek) {
+		this.endWeek = endWeek;
+	}
+
+	public int getEstimatedTime() {
+		return estimatedTime;
+	}
+
+	public void setEstimatedTime(int estimatedTime) {
+		this.estimatedTime = estimatedTime;
+	}
+
 }
