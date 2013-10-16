82ab5a06c3d96d33b6d155b26a9be61ea5a062b3
Olof Spångö <ain09osp@student.lu.se>
2013-10-16 20:56:13 +0200

    stats uppdaterad

--- src/statistics/BurnDown.java
+++ src/statistics/BurnDown.java
@@ -6,12 +6,16 @@ import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import base.servletBase;
-
 public class BurnDown extends StatisticsMenu {
 	public BurnDown() {
 		// Konstruktor
 	}
+	
+	
+	/** 
+	 * 
+	 * 
+	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 		
 	}
@@ -19,11 +23,24 @@ public class BurnDown extends StatisticsMenu {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 		
 	}
+	
+	/**
+	 * Genererar HTML-kod där admin får välja projektgrupp att se BurnDown för.
+	 * 
+	 * 
+	 * @return Sträng med HTML-kod som innehåller formuläret
+	 */
 	private String projectGroupForm() {
 		
 		return null; // returnera html-sträng.
 	}
 	
+	
+	/**
+	 * Genererar html-kod som innehåller BurnDown-plotten.
+	 * @param burnDownData innehåller ***
+	 * @return Sträng som innehåller HTML-kod som visar BurnDown plotten.
+	 */
 	private String printBurnDown(HashMap<String, Integer> burnDownData) {
 		return null;
 	}
--- src/statistics/Statistics.java
+++ src/statistics/Statistics.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import database.*;
-import base.servletBase; // OBS Ska extenda StatisticsMenu egentligen..!
 
 public class Statistics extends StatisticsMenu {
 	public Statistics() {
@@ -24,27 +23,51 @@ public class Statistics extends StatisticsMenu {
 		// hantera om anv. matat in val.
 	}
 	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
 	private String projectGroupForm() {
 		// Samam metod i BurnDown
 		return null; // returnera html-sträng.
 	}
 
-	
+	/**
+	 * 
+	 * 
+	 * @param projectGroup
+	 * @return
+	 */
 	private HashMap<String, ArrayList<String>> getFilter(int projectGroup) {
 
 		return null;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	private HashMap<String, ArrayList<String>> getStats() {
 		//Metod som hämtar användarens inmatade filter. Anropar sedan i sin tur en metod i databasklassen som returnerar Map:en med önskad statistik.		
 		return null;
 	}
 	
+	/**
+	 * 
+	 * @param table
+	 * @return
+	 */
 	private String printTable(HashMap<String, ArrayList<String>> table) {
 		// Metod som skriver ut önskad statistik på tabellform. 
 		return null;
 	}
 	
+	/**
+	 * 
+	 * @param table
+	 * @return
+	 */
 	private String printGraph(HashMap<String, ArrayList<String>> table) {
 		//  Metod för att skriva ut ett stapeldiagram med vecka som x-axel och rapporterad tid på y-axeln.  
 		return null;
