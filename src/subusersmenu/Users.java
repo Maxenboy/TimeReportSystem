c7cf81d5f3e193d413dddf672c0f5673983c985a
Max Åberg <mat09mab@student.lu.se>
2013-10-16 18:03:54 +0200

    Finished implementing Users

--- src/subusersmenu/Users.java
+++ src/subusersmenu/Users.java
@@ -1,20 +1,42 @@
 package subusersmenu;
 
 import database.*;
+
 import java.util.*;
 import java.util.regex.Pattern;
 
 public class Users {
 	private Database db;
 
+	/**
+	 * Constructor
+	 */
 	public Users() {
 		db = new Database();
 	}
 
+	/**
+	 * Generates a form where it is possible to specify a new user.
+	 * @return
+	 */
 	public String userForm() {
-		return null;
+		String html;
+		html = "<p> <form name=" + formElement("input");
+		html += " method=" + formElement("get");
+		html += "<p> Username : <input type=" + formElement("text")
+				+ " name=" + formElement("addname") + '>';
+		html += "<input type=" + formElement("submit") + "value="
+				+ formElement("get username") + '>';
+		html += "</form>";
+		return html;
+	
 	}
 
+	/** 
+	 * Checks if the name is acceptable
+	 * @param name
+	 * @return
+	 */
 	public boolean checkNewName(String name) {
 		if (name.length() < 5 || name.length() > 10
 				|| !Pattern.matches("\\w", name)) {
@@ -23,37 +45,118 @@ public class Users {
 		for (User u : db.getUsers()) {
 			if (u.getUsername().equals(name)) {
 				return false;
-
 			}
 		}
-
 		return true;
 	}
 
+	/**
+	 * Creates a random password
+	 * @return
+	 */
 	public String createPassword() {
-		return null;
+		Random rand = new Random();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 6; i++) {
+			sb.append(Character.toChars(rand.nextInt(25) + 97));
+		}
+		return sb.toString();
 	}
 
+	/**
+	 * Shows all the users
+	 * @param users
+	 * @return
+	 */
 	public String showUsers(ArrayList<User> users) {
-		return null;
+		if (users.isEmpty()) {
+			return null;
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("<FORM METHOD=post ACTION=" + formElement("ShowUsers") + ">");
+		sb.append(buildShowUsersTable());
+		for (User u : users) {
+			sb.append("<tr>");
+			sb.append("<td>" + u.getUsername() + "</td>");
+			sb.append("<td>" + u.getProjectGroup() + "</td>");
+			sb.append("<td>" + u.getRole() + "</td>");
+			sb.append("<td>" + createRadio(u.getId()) + "</td>");
+			sb.append("</tr>");
+		}
+		sb.append("</table>");
+		sb.append("<INPUT TYPE=" + formElement("submit") + "VALUE="
+				+ formElement("Get Users") + ">");
+		sb.append("</form>");
+		return sb.toString();
 	}
 
+	/**
+	 * Makes a user administrator
+	 * @param name
+	 */
 	public void makeAdministrator(String name) {
+		db.getUser(name).setRole(1);
 	}
 
+	/**
+	 * Removes the role administrator from a user
+	 * @param name
+	 */
 	public void unmakeAdministrator(String name) {
+		db.getUser(name).setRole(3);
 	}
 
+	/**
+	 * Deactivates a user
+	 * @param name
+	 * @return
+	 */
 	public boolean deactivateUser(String name) {
-		return false;
+		return db.deactivateUser(db.getUser(name).getId());
 	}
 
+	/**
+	 * Adds a user to the system
+	 * @param name
+	 * @return
+	 */
 	public boolean addUser(String name) {
+		if (checkNewName(name)) {
+			db.addUser(new User(name));
+			return true;
+		}
 		return false;
 	}
 
+	/**
+	 * Activates a user
+	 * @param name
+	 * @return
+	 */
 	public boolean activateUser(String name) {
-		return false;
+		return db.activateUser(db.getUser(name).getId());
+	}
+
+	private String formElement(String par) {
+		return '"' + par + '"';
+	}
+
+	private String createRadio(int id) {
+		return "<input type=" + formElement("radio") + "name="
+				+ formElement("reportId") + "value="
+				+ formElement(Integer.toString(id)) + ">";
+	}
+
+	private String buildShowUsersTable() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<table border=" + formElement("1") + ">");
+		sb.append("<tr>");
+		sb.append("<th>Username</th>");
+		sb.append("<th>Project group</th>");
+		sb.append("<th>Role</th>");
+		sb.append("<th>Select</th>");
+		sb.append("</tr>");
+		return sb.toString();
 	}
 
 }
