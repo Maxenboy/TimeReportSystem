c3042632c8c9127a02ce5b5f2fca35e3b27f2c5e
AlexSoder <ain10aso@student.lu.se>
2013-10-17 17:46:35 +0200

    Implementeringar

--- src/subprojectgroupsmenu/HandleProjectLeader.java
+++ src/subprojectgroupsmenu/HandleProjectLeader.java
@@ -1,15 +1,42 @@
 package subprojectgroupsmenu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.*;
+
+import database.Database;
+
 public class HandleProjectLeader {
 
-	public void NewProjectGroup() {
+	Database db = new Database();
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		HttpSession session = request.getSession();
+		PrintWriter out = response.getWriter();
+
 	}
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
-		
-	}
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
-		
+
+	}
+
+	private String groupForm() {
+		String html;
+		html = "<p> <form name=" + formElement("input");
+		html += " method=" + formElement("get");
+		html += "<p> Which group : <input type=" + formElement("text")
+				+ " name=" + formElement("addname") + '>';
+		html += "<p> Which user : <input type=" + formElement("text") + " status="
+				+ formElement("status") + '>';
+		html += "<input type=" + formElement("submit") + "value="
+				+ formElement("Get group") + '>';
+		html += "</form>";
+		return html;
+	}
+	
+	private String formElement(String par) {
+		return '"' + par + '"';
 	}
 }
