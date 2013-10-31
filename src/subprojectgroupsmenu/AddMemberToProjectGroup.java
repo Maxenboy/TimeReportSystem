a5ad45c2d3f3fcb1d3b4a5476ff093da63ee90b6
AlexSoder <ain10aso@student.lu.se>
2013-10-31 19:14:02 +0100

    max

--- src/subprojectgroupsmenu/AddMemberToProjectGroup.java
+++ src/subprojectgroupsmenu/AddMemberToProjectGroup.java
@@ -2,9 +2,12 @@ package subprojectgroupsmenu;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+
+import database.ProjectGroup;
 
 
 @WebServlet("/AddMemberToProjectGroup")
@@ -25,7 +28,14 @@ public class AddMemberToProjectGroup extends gui.ProjectGroupsMenu {
 				.getAttribute("user_permissions")));
 		out.print(generateSubMenu((Integer) session
 				.getAttribute("user_permissions")));
-		if(request.getParameter("success") == null) {
+		if(request.getParameter("thegroup") == null) {
+			out.print(showProjectGroups());
+		} else {
+			if(groups.showProjectGroup(db.getUsers(Integer.parseInt(request.getParameter("thegroup")))) == null) {
+				
+			}
+		}
+		/*if(request.getParameter("success") == null) {
 			out.print(groups.addUserForm());
 		}
 		else if(request.getParameter("success").equals("true")) {
@@ -35,7 +45,7 @@ public class AddMemberToProjectGroup extends gui.ProjectGroupsMenu {
 		else if (request.getParameter("success").equals("false")) {
 			out.print("<script>$(alert(\"Inkorrekt input.\"))</script>"
 					+ groups.addUserForm());
-		}
+		}*/
 		out.print(getPageOutro());
 	}
 
@@ -53,6 +63,53 @@ public class AddMemberToProjectGroup extends gui.ProjectGroupsMenu {
 			response.sendRedirect(request.getRequestURI() + "?success=false");
 		}
 	}
+	
+	private String showProjectGroups() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<FORM METHOD=" + formElement("get") + ">");
+		sb.append(buildProjectGroupsTable());
+		List<ProjectGroup> groups = db.getProjectGroups();
+		for (ProjectGroup g : groups) {
+			sb.append("<tr>");
+			sb.append("<td>" + g.getProjectName() + "</td>");
+			sb.append("<td>" + g.getStartWeek() + "</td>");
+			sb.append("<td>" + g.getEndWeek() + "</td>");
+			sb.append("<td>" + g.getEstimatedTime() + "</td>");
+			if (g.isActive()) {
+				sb.append("<td>" + "Aktiv" + "</td>");
+			} else {
+				sb.append("<td>" + "Inaktiv" + "</td>");
+			}
+			sb.append("<td>" + createRadio(g.getId()) + "</td>");
+			sb.append("</tr>");
+		}
+		sb.append("</table>");
+		sb.append("<input type=" + formElement("submit") + "value="
+				+ formElement("Aktivera/Inaktivera") + ">");
+		sb.append("</form>");
+		return sb.toString();
+	}
+
+	private String buildProjectGroupsTable() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<table border=" + formElement("1") + ">");
+		sb.append("<tr>");
+		sb.append("<th>Projektgrupp</th>");
+		sb.append("<th>Starvecka</th>");
+		sb.append("<th>Slutvecka</th>");
+		sb.append("<th>Estimerat antal timmar</th>");
+		sb.append("<th>Aktiv</th>");
+		sb.append("<th>Välj</th>");
+		sb.append("</tr>");
+		return sb.toString();
+	}
+
+	private String createRadio(int id) {
+		return "<input type=" + formElement("radio") + "name="
+				+ formElement("thegroup") + "value="
+				+ formElement(Integer.toString(id)) + ">";
+	}
+
 
 	
 }
