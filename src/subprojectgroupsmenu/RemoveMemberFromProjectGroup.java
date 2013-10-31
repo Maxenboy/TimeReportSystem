32d296abc913b74dd20310add0a6380f0fc13ea3
Max Åberg <mat09mab@student.lu.se>
2013-10-31 19:34:57 +0100

    in u're face

--- src/subprojectgroupsmenu/RemoveMemberFromProjectGroup.java
+++ src/subprojectgroupsmenu/RemoveMemberFromProjectGroup.java
@@ -1,12 +1,15 @@
 package subprojectgroupsmenu;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import database.*;
+import database.User;
+import subusersmenu.Users;
 
 @WebServlet("/RemoveMemberFromProjectGroup")
 public class RemoveMemberFromProjectGroup extends gui.ProjectGroupsMenu {
@@ -16,55 +19,36 @@ public class RemoveMemberFromProjectGroup extends gui.ProjectGroupsMenu {
 	 */
 	private static final long serialVersionUID = 4921473121575805469L;
 	ProjectGroups group = new ProjectGroups();
+	Users users = new Users();
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 		HttpSession session = request.getSession();
 		PrintWriter out = response.getWriter();
 		out.print(getPageIntro());
-		out.print(generateMainMenu((Integer) session.getAttribute("user_permissions")));
-		out.print(generateSubMenu((Integer) session.getAttribute("user_permissions")));
-		if (request.getParameter("session") == null) {
-			out.print(removeUserForm());
-		} else if (request.getParameter("session").equals("success")) {
-			out.print(group.showProjectGroup(db.getUsers(Integer
-					.parseInt(request.getParameter("groupid")))));
+		out.print(generateMainMenu((Integer) session
+				.getAttribute("user_permissions")));
+		out.print(generateSubMenu((Integer) session
+				.getAttribute("user_permissions")));
+		if (request.getParameter("username") == null) {
+			out.print(users.showUsers(db.getUsers()));
+			out.print("<script>$('#addusertogroup').submit(function (e) { e.preventDefault(); var confirmed = confirm(\"Är du säker?\");if (confirmed) {$(this).submit();}});</script>");
 		} else {
-			out.print("<script>$(alert(\"Kunde inte lägga till grupp\"))</script>"
-					+ removeUserForm());
+			User user = db.getUser(Integer.parseInt(request
+					.getParameter("username")));
+			if (user.getProjectGroup() != 0) {
+				db.removeUserFromProjectGroup(user.getId(),
+						user.getProjectGroup());
+			} else {
+				out.print("<script>$(alert(\"Användaren har ingen projektgrupp och kan därför inte tas bort ur en\"))</script>");
+			}
 		}
 		out.print(getPageOutro());
-
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
-		HttpSession session = request.getSession();
-		String userName = request.getParameter("name");
-		String groupId = request.getParameter("groupid");
-		if (group.removeUserFromProjectGroup(userName,
-				Integer.parseInt(groupId))) {
-			response.sendRedirect(request.getRequestURI() + "?session=success");
-		} else {
-			response.sendRedirect(request.getRequestURI()
-					+ "?session=false&groupid=" + groupId);
-		}
+		
 	}
 
-	private String removeUserForm() {
-		String html;
-		html = "<p> <form name=" + formElement("input") + "id="
-				+ formElement("removeuserfromgroup");
-		html += " method=" + formElement("get");
-		html += "<p> Anv�ndarnamn: <input type=" + formElement("text") + " name="
-				+ formElement("name") + '>';
-		html += "<p> Grupp id: <input type=" + formElement("text")
-				+ " groupid=" + formElement("groupid") + '>';
-		html += "<input type=" + formElement("submit") + "value="
-				+ formElement("Ta bort anv�ndare") + '>';
-		html += "</form>";
-		return html;
-	}
-
-	
 }
