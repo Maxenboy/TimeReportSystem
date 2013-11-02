3708639875e59d82f79d2b5b3f9bc5fb372c2769
Andreas Olsson <olsson.andreas@gmail.com>
2013-11-02 11:24:11 +0100

    Some stability fixes to WelcomePage

--- src/gui/WelcomePage.java
+++ src/gui/WelcomePage.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import database.User;
+
 import base.servletBase;
 
 
@@ -46,16 +48,19 @@ public class WelcomePage extends servletBase {
 		if (!loggedIn(request)) {
 			session.setAttribute("state", LOGIN_FALSE);
 			response.sendRedirect("LogIn");
+		} else {
+			out.print(getPageIntro());
+			int userPermission = User.ROLE_NO_ROLE;
+			if(session.getAttribute("user_permissions") != null) {
+				userPermission = (Integer) session.getAttribute("user_permissions");			
+			}
+			out.append(generateMainMenu(userPermission, request));
+			out.println("<div class='container'><div class='row'>");
+			out.println("<div class='col-lg-12'><h1>V\u00E4lkommen</h1><p>TidRapporteringsSystem av grupp 2 i Programvaruutveckling i stora system 2013</div>");
+			out.println("</div></div>");
+			out.println(getPageOutro());
 		}
 		
-		out.print(getPageIntro());
-		int userPermission = (Integer) session.getAttribute("user_permissions");
-		out.append(generateMainMenu(userPermission, request));
-		
-		out.println("<div class='container'><div class='row'>");
-		out.println("<div class='col-lg-12'><h1>V\u00E4lkommen</h1><p>TidRapporteringsSystem av grupp 2 i Programvaruutveckling i stora system 2013</div>");
-		out.println("</div></div>");
-		out.println(getPageOutro());
 	}
 
 }
