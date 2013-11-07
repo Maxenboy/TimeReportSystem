3077c93565ca90016c74d24c7241eb5a87912e1c
Andreas Olsson <olsson.andreas@gmail.com>
2013-11-07 15:53:55 +0100

    Destroyed the whole login, fixed now

--- src/gui/WelcomePage.java
+++ src/gui/WelcomePage.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import base.ServletBase;
+import database.ProjectGroup;
 import database.User;
 
 
@@ -56,7 +57,7 @@ public class WelcomePage extends ServletBase {
 			out.append(generateMainMenu(userPermission, request));
 			out.println("<div class='container'><div class='row'>");
 			out.println("<div class='col-lg-12'><h1>V\u00E4lkommen</h1><p>TidRapporteringsSystem av grupp 2 i Programvaruutveckling f\u00F6r stora system 2013</div>");
-			if(userPermission == User.ROLE_NO_ROLE) {
+			if(((int) session.getAttribute("project_group_id") == 0) && (userPermission != User.ROLE_ADMIN)) {
 				out.println("<div class='col-lg-12'><p style='color: red;'>Du \u00E4r inte indelad i n\u00E5gon projektgrupp i TidRapporteringsSystemet. Kontakta administrat\u00F6r</p></div>");
 			}
 			out.println("</div></div>");
