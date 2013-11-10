9c80b0c75759bfaa5c062c1a4967dfe49d22f623
AlexSoder <ain10aso@student.lu.se>
2013-11-10 14:01:40 +0100

    Pust

--- src/subusersmenu/ActiveStatusForUser.java
+++ src/subusersmenu/ActiveStatusForUser.java
@@ -33,7 +33,7 @@ public class ActiveStatusForUser extends UsersMenu {
 			out.print(generateSubMenu(permission));
 
 			if (request.getParameter("username") == null) {
-				out.print(showUsers(db.getUsers()));
+				out.print(showUsers(db.getUsers(), (Integer) session.getAttribute("id")));
 				out.print("<script>$('#addusertogroup').submit(function (e) { e.preventDefault(); var confirmed = confirm(\"\u00C4r du s\u00F6ker?\");if (confirmed) {$(this).submit();}});</script>");
 			} else {
 				User user = db.getUser(Integer.parseInt(request
@@ -42,14 +42,14 @@ public class ActiveStatusForUser extends UsersMenu {
 					if (user.isActive()) {
 						db.deactivateUser(user.getId());
 						out.print("<script>$('#addusertogroup').submit(function (e) { e.preventDefault(); var confirmed = confirm(\"\u00C4r du s\u00F6ker?\");if (confirmed) {$(this).submit();}});</script>");
-						out.print(showUsers(db.getUsers()));
+						out.print(showUsers(db.getUsers(), (Integer) session.getAttribute("id")));
 					} else {
 						db.activateUser(user.getId());
-						out.print(showUsers(db.getUsers()));
+						out.print(showUsers(db.getUsers(), (Integer) session.getAttribute("id")));
 					}
 				} else {
 					out.print("<script>$(alert(\"Du kan inte inaktivera dig sj\u00E4lv!\"))</script>");
-					out.print(showUsers(db.getUsers()));
+					out.print(showUsers(db.getUsers(), (Integer) session.getAttribute("id")));
 				}
 			}
 			out.print(getPageOutro());
@@ -63,7 +63,7 @@ public class ActiveStatusForUser extends UsersMenu {
 
 	}
 
-	private String showUsers(ArrayList<User> users) {
+	private String showUsers(ArrayList<User> users, int id) {
 		if (users.isEmpty()) {
 			return null;
 		}
@@ -84,7 +84,7 @@ public class ActiveStatusForUser extends UsersMenu {
 			sb.append("<td>" + translateRole(u.getRole()) + "</td>");
 			String active = u.isActive() ? "Aktiv" : "Inaktiv";
 			sb.append("<td>" + active + "</td>");
-			if (u.getRole() != User.ROLE_ADMIN) {
+			if (u.getId() != id) {
 				sb.append("<td>" + createRadio(u.getId()) + "</td>");
 			} else {
 				sb.append("<td>" + "</td>");
